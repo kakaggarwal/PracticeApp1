91a2bf80916abf64f6f1151cc49d2ae2bc557373
Krishan Aggarwal <kakaggarwal@gmail.com>
2025-01-23 16:17:30 +0530

    Angular version updated to v19

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,9 +4,10 @@ import { Router } from "@angular/router";
 import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss'],
+    standalone: false
 })
 
 export class AboutComponent implements OnInit {
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,33 +3,31 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
 import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
-  animations:[
-    trigger('goals', [
-      transition('* => *', [
-        query(':enter', style({ opacity: 0 }), { optional: true }),
-
-        query(':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)', offset: 1}),
-          ]))
-        ]), { optional: true }),
-
-        query(':leave', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: 0.5, transform: 'translateY(35px)', offset: 0.3}),
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 1}),
-          ]))
-        ]), { optional: true })
-
-      ])
-    ])
-  ]
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
+    animations: [
+        trigger('goals', [
+            transition('* => *', [
+                query(':enter', style({ opacity: 0 }), { optional: true }),
+                query(':enter', stagger('300ms', [
+                    animate('.6s ease-in', keyframes([
+                        style({ opacity: 0, transform: 'translateY(-75%)', offset: 0 }),
+                        style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+                        style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+                    ]))
+                ]), { optional: true }),
+                query(':leave', stagger('300ms', [
+                    animate('.6s ease-in', keyframes([
+                        style({ opacity: 1, transform: 'translateY(0)', offset: 0 }),
+                        style({ opacity: 0.5, transform: 'translateY(35px)', offset: 0.3 }),
+                        style({ opacity: 0, transform: 'translateY(-75%)', offset: 1 }),
+                    ]))
+                ]), { optional: true })
+            ])
+        ])
+    ],
+    standalone: false
 })
 
 export class HomeComponent implements OnInit {
